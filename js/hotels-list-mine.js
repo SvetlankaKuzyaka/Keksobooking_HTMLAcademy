41d184e9367834c147885617d1e9d31bb9841350
SvetlankaKuzyaka <svetlanka-kuznecov@yandex.ru>
2017-08-20 20:31:29 +0300

    Changes_Lesson4_3

--- js/hotels-list-mine.js
+++ js/hotels-list-mine.js
@@ -10,32 +10,37 @@
   });
 
   function getElementFromTemplate(data) {
-    var IMAGE_TIMEOUT = 10000;
     var template = document.querySelector('#hotel-template');
+
     if ('content' in template) {
       var element = template.content.children[0].cloneNode(true);
     } else {
       var element = template.children[0].cloneNode(true);
-    };
+    }
+
     element.querySelector('.hotel-name').textContent = data.name;
     element.querySelector('.hotel-rating').textContent = data.rating;
     element.querySelector('.hotel-price-value').textContent = data.price;
 
     var backgroundImage = new Image();
-    var imageLoadTimeout = setTimeOut(function() {
-      backgroundImage.src = '';
-      element.classList.add('hotel-nophoto');
-    }, IMAGE_TIMEOUT);
+
     backgroundImage.onload = function() {
       clearTimeOut(imageLoadTimeout);
       element.style.backgroundImage = 'url(\'' + backgroundImage.src + '\')';
     };
+
     backgroundImage.onerror = function() {
       element.classList.add('hotel-nophoto');
     };
-    backgroundImage.src = '/' + data.preview;
-    
-    return element;
-  };
 
+    var IMAGE_TIMEOUT = 10000;
+    var imageLoadTimeout = setTimeOut(function() {
+      backgroundImage.src = '';
+      element.classList.add('hotel-nophoto');
+    }, IMAGE_TIMEOUT);
+
+    backgroundImage.src = data.preview;
+
+    return element;
+  }
 })();
