45840b255c14477a871d89f1965d1162db629681
SvetlankaKuzyaka <svetlanka-kuznecov@yandex.ru>
2017-10-31 12:06:19 +0300

    Changes_in_general_4

--- js/hotel-data.js
+++ js/hotel-data.js
@@ -7,13 +7,26 @@
    HotelData.prototype.getPictures = function() {
       return this.params.pictures;
    };
-
    HotelData.prototype.getPrice = function() {
       return this.params.price;
    };
-
    HotelData.prototype.setPrice = function(price) {
       this.params.price = price;
+   };
+   HotelData.prototype.getName = function() {
+      return this.params.name;
+   };
+   HotelData.prototype.getRating = function() {
+      return this.params.rating;
+   };
+   HotelData.prototype.getStars = function() {
+      return this.params.stars;
+   };
+   HotelData.prototype.getDistance = function() {
+      return this.params.distance;
+   };
+   HotelData.prototype.getAmenities = function() {
+      return this.params.amenities;
    };
 
    window.HotelData = HotelData;
